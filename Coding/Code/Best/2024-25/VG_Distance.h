0e45345bb16ccadcd70c439a2754f95e4375402b
Bluedogman <james.gordon@students.nisd.net>
2024-10-15 17:19:39 -0500

    idk what this is hopefully im not screwing this up

--- Coding/Code/Best/2024-25/VG_Distance.h
+++ Coding/Code/Best/2024-25/VG_Distance.h
@@ -3,24 +3,34 @@
 float segmentArc = 360 / wheelSegments;
 float segmentArcLength = segmentArc * wheelDiameter * PI / 360;
 bool segment = false;
+bool segment2 = false;
 
 float distCalc(float distance) {
 	if (SensorValue[IR] < 200 && false == segment) {
 		distance += segmentArcLength;
 		segment = true;
-	} else if (SensorValue[IR] > 200) {
+		} else if (SensorValue[IR] > 200) {
 		segment = false;
 	}
 	return distance;
 }
-
-void distDrive(bool button, float totalDistance, int speed) {
-	if (true == button) {
-		int distanceGone = 0;
-		while (distanceGone < totalDistance) {
-			setMultipleMotors(speed, LDrive, RDrive);
-			distanceGone = distCalc(distanceGone);
+	float distCalc2(float distance2) {
+		if (SensorValue[IR2] < 200 && false == segment2) {
+			distance2 += segmentArcLength;
+			segment2 = true;
+			} else if (SensorValue[IR2] > 200) {
+			segment2 = false;
 		}
-		stopAllMotors();
+		return distance2;
 	}
-}
+
+	void distDrive(bool button, float totalDistance, int speed) {
+		if (true == button) {
+			int distanceGone = 0;
+			while (distanceGone < totalDistance) {
+				setMultipleMotors(speed, LDrive, RDrive);
+				distanceGone = distCalc(distanceGone);
+			}
+			stopAllMotors();
+		}
+	}
