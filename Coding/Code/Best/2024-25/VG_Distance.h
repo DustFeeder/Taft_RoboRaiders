d387fc0cd53c29653b4ab0940681fced5bbe6b06
Bluedogman <james.gordon@students.nisd.net>
2024-10-26 10:31:39 -0500

    Maybe finished auto foundation

--- Coding/Code/Best/2024-25/VG_Distance.h
+++ Coding/Code/Best/2024-25/VG_Distance.h
@@ -12,13 +12,33 @@ float motionCalculation(float distance, float encoder, bool turning) {
 	return distance;
 }
 
-void autoDrive(bool turning, bool button, float encoder, float motor, int speed, float totalDistance) {
-	if (true == button) {
-		int distanceGone = 0;
-		while (distanceGone < totalDistance) {
-			setMotor(motor, speed);
-			distanceGone = motionCalculation(distanceGone, encoder, turning);
-		}
-		setMotor(motor, 0);
+//distance is in inches
+float autoDrive(bool turning, float encoder, float motor, int speed, float distanceGone) {
+	setMotor(motor, speed);
+	distanceGone = motionCalculation(distanceGone, encoder, turning);
+	return distanceGone;
+}
+
+float driveControl(bool turning, float encoder, float motor, int speed, float target, float distanceGone) {
+	if (distanceGone < target) {
+		setMotor(motor, speed);
+	} else {
+		stopMotor(motor);
+	}
+	distanceGone = motionCalculation(distanceGone, encoder, turning);
+	return distanceGone;
+}
+
+void autoDrive2(bool turning, int speed, float target) {
+	float distanceL = 0;
+	float distanceR = 0;
+	float targetL = target;
+	float targetR = target;
+	if (turning) {
+		targetR = -target;
+	}
+	while (distanceL < targetL || distanceR < targetR) {
+		distanceL = driveControl(turning, IRL, LDrive, speed, targetL, distanceL);
+		distanceR = driveControl(turning, IRR, RDrive, speed, targetR, distanceR);
 	}
 }
--- Coding/Code/Best/2024-25/VG_robotDimensions.h
+++ Coding/Code/Best/2024-25/VG_robotDimensions.h
@@ -1,8 +1,8 @@
 //we're using inches, final diameter is 6.625 inches
-int segmentCount = 34;		//test value
+int segmentCount = 9;		//test value
 float wheelDiameter = 12;	//test value
-float wheelBaseWidth = 0;	//find actual value
+float wheelBaseWidth = 15 + 3/8;	//find actual value
 
-float segmentArcLength = wheelDiameter * 3.14 / segmentCount;	//replace "3.14" with "PI" in robotC
+float segmentArcLength = wheelDiameter * PI / segmentCount;	//replace "3.14" with "PI" in robotC
 float segmentArcDegrees = (360 * wheelDiameter) / (segmentCount * wheelBaseWidth);
 bool segment = false;
